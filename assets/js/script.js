98fcbb932b6213d784fbcf4c67951a53f9f00693
Mildred Aneke <mildylicious@gmail.com>
2024-11-05 10:25:09 +0000

    Write the questions of project on javascript

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+const questions = [
+    {
+        question: "Who wrote the novel 'Pride & Pejudice'?",
+        answers: [
+            { text: "J.D Salinger", correct: false },
+            { text: "Leo Tolstoy", correct: false },
+            { text: "F. Scott Fitzgerald", correct: false },
+            { text: "Jane Austen", correct: true }
+        ]
+    },
+    {
+        question: "Which artist painted the 'Mona Lisa'?",
+        answers: [
+            { text: "Vincent van Gogh", correct: false },
+            { text: "Leonardo da Vinci", correct: true },
+            { text: "Pablo Picasso", correct: false },
+            { text: "Claude Monet", correct: false }
+        ]
+    },
+    {
+        question: "Who wrote 'Alice Adventure in Wonderland'?",
+        answers: [
+            { text: "George Orwell", correct: false },
+            { text: "Claude Monet", correct: false },
+            { text: "Lewis Carroll", correct: true },
+            { text: "Moby Dick", correct: false }
+        ]
+    },
+    {
+        question: "In Greek mythology, who was the god of the sea?",
+        answers: [
+            { text: "Poseidon", correct: true },
+            { text: "Cronos", correct: false },
+            { text: "Neptune", correct: false },
+            { text: "Pluto", correct: false }
+        ]
+    },
+    {
+        question: "In the play 'Romeo & Juliet',who is Juliet's romantic partner?",
+        answers: [
+            { text: "Rio", correct: false },
+            { text: "Samuel", correct: false },
+            { text: "Romeo", correct: true },
+            { text: "David", correct: false }
+        ]
+    },
+    {
+        question: "Which figure appears in the da Vinci picture 'Salvator Mundi'?",
+        answers: [
+            { text: "Jesus", correct: true },
+            { text: "Mary", correct: false },
+            { text: "HolySpirit", correct: false },
+            { text: "Moses", correct: false }
+        ]
+    },
+];
