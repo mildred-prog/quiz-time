82ffde9da676f9f03b25f5562a33cefb6877027f
Mildred Aneke <mildylicious@gmail.com>
2024-11-08 13:16:57 +0000

    Set up structure for quiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,124 @@
-// Variables to keep track of quiz progress and scores
-let currentQuestionIndex = 0;
-let score = 0;
-
-// Variables to keep track of quiz state
-const startQuizButton = document.getElementById("startQuiz");
-const viewRecentScoresButton = document.getElementById("viewRecentScores");
-const openSettingsButton = document.getElementById("openSettings");
-const usernameInput = document.getElementById("username");
-
-// Array containing quiz data (questions, choices, and answers)
+// Array containing quiz data (questions, options, and correct answers)
 const questions = [
     {
         question: "Who wrote the novel 'Pride & Pejudice'?",
-        answers: [
-            { text: "J.D Salinger", correct: false },
-            { text: "Leo Tolstoy", correct: false },
-            { text: "F. Scott Fitzgerald", correct: false },
-            { text: "Jane Austen", correct: true }
-        ]
+        options: [
+          "J.D Salinger", 
+          "Leo Tolstoy",
+          "F. Scott Fitzgerald",
+          "Jane Austen",
+        ],
+        correctAnswer: "Jane Austen",
     },
     {
+        question: "Which metallic color is used on the background of 'The Kiss' by Klimt?",
+        options: [
+          "Red",
+          "Yellow",
+          "Purple",
+          "Gold",
+        ],
+        correctAnswer: "Gold",
+      },
+    {
         question: "Which artist painted the 'Mona Lisa'?",
-        answers: [
-            { text: "Vincent van Gogh", correct: false },
-            { text: "Leonardo da Vinci", correct: true },
-            { text: "Pablo Picasso", correct: false },
-            { text: "Claude Monet", correct: false }
-        ]
+        options: [
+          "Vincent van Gogh",
+          "Leonardo da Vinci",
+          "Pablo Picasso",
+          "Claude Monet",
+        ],
+        correctAnswer: "Leonardo da Vinci",
     },
     {
         question: "Who wrote 'Alice Adventure in Wonderland'?",
-        answers: [
-            { text: "George Orwell", correct: false },
-            { text: "Claude Monet", correct: false },
-            { text: "Lewis Carroll", correct: true },
-            { text: "Moby Dick", correct: false }
-        ]
+        options: [
+          "George Orwell",
+          "Claude Monet",
+          "Lewis Carroll",
+          "Moby Dick",
+        ],
+        correctAnswer: "Lewis Carroll",
     },
     {
         question: "In Greek mythology, who was the god of the sea?",
-        answers: [
-            { text: "Poseidon", correct: true },
-            { text: "Cronos", correct: false },
-            { text: "Neptune", correct: false },
-            { text: "Pluto", correct: false }
-        ]
+        options: [
+            "Poseidon",
+            "Cronos",
+            "Neptune",
+            "Pluto",
+        ],
+        correctAnswer: "Poseidon",
     },
     {
         question: "In the play 'Romeo & Juliet',who is Juliet's romantic partner?",
-        answers: [
-            { text: "Rio", correct: false },
-            { text: "Samuel", correct: false },
-            { text: "Romeo", correct: true },
-            { text: "David", correct: false }
-        ]
+        options: [
+            "Rio",
+            "Samuel",
+            "Romeo",
+            "David",
+        ],
+        correctAnswer: "Romeo",
     },
     {
         question: "Which figure appears in the da Vinci picture 'Salvator Mundi'?",
-        answers: [
-            { text: "Jesus", correct: true },
-            { text: "Mary", correct: false },
-            { text: "HolySpirit", correct: false },
-            { text: "Moses", correct: false }
-        ]
+        options: [
+            "Jesus",
+            "Mary",
+            "HolySpirit",
+            "Moses",
+        ],
+        correctAnswer: "Jesus",
     },
-
-    
+    {
+        question: " Who is the author of the dystopian novel '1984'?",
+        options: [
+            "George R.R. Martin",
+            "George Orwell",
+            "Daren Salinger",
+            "Lewis Carroll",
+        ],
+        correctAnswer: "George Orwell",
+    },
+    {
+        question: " Who wrote the novel 'The Catcher in the Rye'?",
+        options: [
+            "Stuart Martin",
+            "Karl Marx",
+            "J.D. Salinger",
+            "Paul Carroll",
+        ],
+        correctAnswer: "J.D. Salinger",
+    },    
+    {
+        question: "How many lines are there in a Shakesperean sonnet?",
+        options: [
+            "10",
+            "12",
+            "14",
+            "16",
+        ],
+        correctAnswer: "14",
+    },     
 ];
+
+const welcomeContainer = document.getElementById("welcome-container");
+const gameContainer = document.getElementById("game-container");
+const resultsContainer = document.getElementById("results-container");
+//Grabbing the answer btn and setting the variable
+const btnContainers = document.getElementsByClassName("answers-sub-container");
+const playBtn = document.getElementById("play-btn");
+//Variable for shuffled questions undefined enabling us to define inside the function startQuiz
+let shuffledListOfQuestionAndAnswers;
+//Grabbing the round number element for setting the round number
+const roundNumber = document.getElementById("round-number");
+//Grabbing the question <p> element for setting the question
+const questionElement = document.getElementById("question");
+//Grabbing btn elements for next question and restarting the quiz
+const nextRoundBtn = document.getElementById("next-round-btn");
+const restartBtn = document.getElementById("restart-quiz");
+//Grabbing the results container text variables
+const endQuizTitle = document.getElementById("encouragement-title");
+let correctAnswers = document.getElementById("correct-answers");
+const scoreText = document.getElementById("score-text");
 
